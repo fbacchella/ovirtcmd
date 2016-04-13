1b169cc73da88a613cfaa6000fc8301cee157393
Fabrice Bacchella <fbacchella@spamcop.net>
2016-04-13 15:20:29 +0200

    Full capabilities enumeration and discover

--- ovlib/capabilities/__init__.py
+++ ovlib/capabilities/__init__.py
@@ -7,20 +7,36 @@ class_ref = []
 class List(ovlib.verb.List):
     verb = "list"
 
+    def execute(self, *args, **kwargs):
+        for i in self.contenaire.list():
+            yield "%d.%d %s " % (i.get_major(), i.get_minor(), i.get_id())
+
 @add_command(class_ref)
 class XmlExport(ovlib.verb.XmlExport):
     verb = "export"
 
-    def execute(self, *args, **kwargs):
-        #output = StringIO()
-        #self.broker.export_(output, 0)
-        #return output.getvalue()
-        #for k in dir(self.broker):
-        #    v = getattr(self.broker, k)
-        #    print "%s %s" % (k, v)
-        self.broker.current = None
-        self.broker.features = None
-        return super(XmlExport, self).execute(*args, **kwargs)
+
+class CapabilitiesContext(Object_Context):
+    def __init__(self, object_name, api_attribute, commands):
+        super(CapabilitiesContext, self).__init__(object_name, api_attribute, commands)
+        self.allcapa = None
+
+    def fill_parser(self, parser):
+        parser.add_option("-i", "--id", dest="id", help="object ID")
+        parser.add_option("-v", "--version", dest="version", help="capabilities version major.minor")
+
+    # needed to work around bug https://bugzilla.redhat.com/show_bug.cgi?id=1326729
+    def get(self, **kwargs):
+        id = kwargs.pop('id', None)
+        version = kwargs.pop('version', None)
+        if self.allcapa is None:
+            self.allcapa = self.execute("list")
+        for i in self.allcapa:
+            if id is not None and i.get_id() == id:
+                return i
+            elif version is not None and ("%d.%d" %(i.get_major(), i.get_minor())) == version:
+                return i
+        return None
 
 
-oc = Object_Context(api_attribute = "capabilities", object_name = "capa", commands = class_ref)
+oc = CapabilitiesContext(api_attribute = "capabilities", object_name = "capa", commands = class_ref)
