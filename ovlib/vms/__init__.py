7ad1fea319e61e397afb314bd8bf049002094120
Fabrice Bacchella <fbacchella@spamcop.net>
2016-04-13 19:00:33 +0200

    Added some crude console URL generation

--- ovlib/vms/__init__.py
+++ ovlib/vms/__init__.py
@@ -2,6 +2,7 @@ import ovlib.verb
 import create
 import delete
 import autoinstall
+import urllib
 from ovlib import Object_Context, add_command
 
 class_ref = []
@@ -27,6 +28,29 @@ class Start(ovlib.verb.XmlExport):
 
     def execute(self, *args, **kwargs):
         self.broker.start()
+
+@add_command(class_ref)
+class Start(ovlib.verb.XmlExport):
+    verb = "ticket"
+
+    def execute(self, *args, **kwargs):
+        ticket = self.broker.ticket().ticket
+        console_info = {
+            'type': self.broker.display.type_,
+            'address': self.broker.display.address,
+            'password': ticket.value,
+            'port': self.broker.display.port,
+            'secure_port': self.broker.display.secure_port,
+            'title': self.broker.name + ":%d"
+        }
+        for (k,v) in console_info.items():
+            console_info[k] = urllib.quote(str(v))
+        if console_info['type'] == 'spice':
+            url = "spice://{address}:{port}/?password={password}&tls-port={secure_port}".format(**console_info)
+        elif console_info['type'] == 'vnc':
+            url = "vnc://{address}:{port}".format(**console_info)
+
+        return url
 
 
 class_ref.append(create.Create)
