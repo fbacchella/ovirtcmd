7268a35df8315da433e20787e2c2d1e1254889c3
Fabrice Bacchella <fbacchella@spamcop.net>
2016-05-04 18:38:39 +0200

    Cluster creation

--- ovlib/clusters/__init__.py
+++ ovlib/clusters/__init__.py
@@ -19,4 +19,29 @@ class Delete(ovlib.verb.Verb):
     def execute(self, *args, **kwargs):
         self.broker.delete()
 
+
+@add_command(class_ref)
+class Create(ovlib.verb.Verb):
+    verb = "create"
+
+    def uses_template(self):
+        return True
+
+    def fill_parser(self, parser):
+        parser.add_option("-n", "--name", dest="name", help="New cluster name", default=None)
+
+    def validate(self):
+        return True
+
+    def execute(self, *args, **kwargs):
+        cpu_type = kwargs.pop('cpu_type', None)
+        if cpu_type is not None:
+            kwargs['cpu'] = params.CPU(id=cpu_type)
+
+        dc_name = kwargs.pop('dc_name', None)
+        if dc_name is not None:
+            kwargs['data_center'] = self.api.datacenters.get(dc_name)
+
+        self.broker = self.contenaire.add(params.Cluster(**kwargs))
+
 content = Object_Context(api_attribute ="clusters", object_name ="cluster", commands = class_ref)
