7bd661f210d342486f4efc50c6cea77513a6da1d
Fabrice Bacchella <fbacchella@spamcop.net>
2016-05-06 10:31:48 +0200

    Added delete to clusters, improved memery settings in cluster creation

--- ovlib/clusters/__init__.py
+++ ovlib/clusters/__init__.py
@@ -42,6 +42,29 @@ class Create(ovlib.verb.Verb):
         if dc_name is not None:
             kwargs['data_center'] = self.api.datacenters.get(dc_name)
 
+        memory_policy = kwargs.pop('memory_policy', None)
+        if isinstance(memory_policy, dict):
+            if 'overcommit' in memory_policy:
+                memory_policy['overcommit'] = params.MemoryOverCommit(percent=memory_policy['overcommit'])
+            if 'transparent_hugepages' in memory_policy:
+                memory_policy['transparent_hugepages'] = params.TransparentHugePages(enabled=memory_policy['transparent_hugepages']==True)
+            kwargs['memory_policy'] = params.MemoryPolicy(**memory_policy)
+        else:
+            overcommit = kwargs.pop('overcommit', None)
+            ballooning  = kwargs.pop('ballooning', None)
+            transparent_hugepages = kwargs.pop('transparent_hugepages', None)
+            if overcommit  is not None:
+                pass
+
         self.broker = self.contenaire.add(params.Cluster(**kwargs))
+
+
+@add_command(class_ref)
+class Delete(ovlib.verb.Verb):
+    verb = "delete"
+
+    def execute(self, *args, **kwargs):
+        return self.broker.delete()
+
 
 content = Object_Context(api_attribute ="clusters", object_name ="cluster", commands = class_ref)
