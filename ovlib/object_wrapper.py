994296712bfb976975f13ef89c03a899377f5ff8
Fabrice Bacchella <fbacchella@spamcop.net>
2016-05-06 18:03:05 +0200

    Removed useless class

--- ovlib/object_wrapper.py
+++
@@ -1,7 +0,0 @@
-
-class Object_Wrapper(object):
-    attribute = None
-
-    """A abstract class, used to implements actual ec2 objects"""
-    def __init__(self, context, **kwargs):
-        self.context = context
