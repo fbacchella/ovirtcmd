7759490c141893399df01b66384c79f9520e4f93
Fabrice Bacchella <fbacchella@spamcop.net>
2016-05-16 19:00:23 +0200

    Working bonding, but don't manage the IP.

--- ovlib/hosts/bond.py
+++ ovlib/hosts/bond.py
@@ -11,15 +11,12 @@ class Bond(ovlib.verb.Verb):
     def fill_parser(self, parser):
         parser.add_option("-i", "--interface", dest="interfaces", help="Interface to add to bonding", default=[], action="append")
         parser.add_option("-m", "--mtu", dest="mtu", help="MTU for the interface", type=int)
-        parser.add_option("-n", "--network", dest="network", help="Network to bond", default='ovirtmgmt')
-        parser.add_option("-o", "--bond_option", dest="bond_options", action="store_variable", type="string")
+        parser.add_option("-n", "--network", dest="networks", help="Network to bond", default=[], action="append")
+        parser.add_option("-o", "--bond_option", dest="bond_options", action="store_variable", type="string", help="used as '-o name value'")
         parser.add_option("-b", "--bond_name", dest="bond_name", default="bond0")
-        parser.add_option("-I", "--ip", dest="ip")
-        parser.add_option("-G", "--gateway", dest="gateway")
-        parser.add_option("-D", "--dhcp", dest="dhcp", action="store_true", default=True, help="configure this interface using DHCP")
-        parser.add_option("-K", "--keep", dest="keep", help="Keep IP information for this interface")
 
     def execute(self, *args, **kwargs):
+        # build the bonded interfaces
         bond_name = kwargs['bond_name']
 
         mtu = kwargs.pop('mtu', None)
@@ -33,30 +30,33 @@ class Bond(ovlib.verb.Verb):
         )
         bonded_if = params.HostNIC(name=bond_name, bonding=bonding, mtu=mtu)
 
-        ip = kwargs.pop('ip', None)
-        gateway = kwargs.pop('gateway', None)
-        keep = kwargs.pop('keep', None)
-        dhcp = kwargs.pop('dhcp', False)
-        if keep is not None:
-            old_ip_nic = self.broker.nics.get(name=keep)
-            ip_assignment = old_ip_nic.networkattachments.list()[0].ip_address_assignments
-        elif ip is not None:
-            ip = unicode(ip)
-            ip_addr = ipaddress.ip_address(ip.split('/')[0])
-            ip_net = ipaddress.ip_network(ip, strict=False)
-            ip_conf = params.IP(address=str(ip_addr),
-                                netmask=str(ip_net).split('/')[0])
-            if gateway is not None:
-                ip_conf.set_gateway(gateway)
-            ip_assignment = params.IpAddressAssignments([params.IpAddressAssignment(assignment_method="static", ip=ip_conf)])
-        elif dhcp is True:
-            ip_assignment = params.IpAddressAssignments([params.IpAddressAssignment(assignment_method="dhcp", ip=params.IP())])
+        # resolves the network that needs to be moved to this interface
+        # IP configuration is kept if network already exists
+        old_net_attachement = {}
+        networks_by_name = {}
+        for i in self.broker.networkattachments.list():
+            net = self.get(self.api.networks, i.network.id)
+            old_net_attachement[net.name] = {'id': i.id, 'ips': i.ip_address_assignments}
+            networks_by_name[net.name] = net.id
 
-        bonded_network = params.NetworkAttachment(network=params.Network(name=kwargs['network']),
-                                                  host_nic=params.HostNIC(name=bond_name),
-                                                  ip_address_assignments=ip_assignment)
+        bonded_networks_list = kwargs.pop('networks', [])
+        if len(bonded_networks_list) == 0:
+            bonded_networks_list = ['ovirtmgmt']
 
-        return self.broker.setupnetworks(params.Action(modified_bonds=params.HostNics(host_nic = [bonded_if]),
-                                                       modified_network_attachments=params.NetworkAttachments(network_attachment=[bonded_network]),
+        bonded_networks = []
+        bond_nic = params.HostNIC(name=bond_name)
+        for i in bonded_networks_list:
+            attachement_kwargs = {'host_nic':  bond_nic }
+            if i in networks_by_name:
+                attachement_kwargs['network'] = params.Network(id=networks_by_name[i])
+            else:
+                attachement_kwargs['network'] = params.Network(id=self.get(self.api.networks, i).id)
+            if i in old_net_attachement:
+                attachement_kwargs['id'] = old_net_attachement[i]['id']
+                attachement_kwargs['ip_address_assignments'] = old_net_attachement[i]['ips']
+            bonded_networks.append(params.NetworkAttachment(**attachement_kwargs))
+
+        return self.broker.setupnetworks(params.Action(modified_bonds=params.HostNics(host_nic=[bonded_if]),
+                                                       modified_network_attachments=params.NetworkAttachments(network_attachment=bonded_networks),
                                                        )
                                          )
